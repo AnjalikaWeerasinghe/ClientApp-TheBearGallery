0d5d4e1fb913801c7bf5f6248e7242308e22c285
AnjalikaWeerasinghe <148033939+AnjalikaWeerasinghe@users.noreply.github.com>
2024-01-21 03:18:45 +0530

    Update Home, Main Window

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -6,18 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-/*
-  userspecmessages: any[] = [
-    {name: 'ashan.d@earth.lk', updated: new Date('5/30/23')},
-    {name: 'rukmal.d@earth.lk', updated: new Date('5/17/23')},
-    {name: 'it@earth.lk', updated: new Date('5/28/23')},
-    {name: 'it@earth.lk', updated: new Date('4/28/23')},
-  ];
 
-  generalmessages: any[] = [
-    {name: 'hr@earth.lk', updated: new Date('5/30/23')},
-    {name: 'admin@earth.lk', updated: new Date('5/17/23')},
-    {name: 'it@earth.lk', updated: new Date('5/28/23')},
-    {name: 'it@earth.lk', updated: new Date('4/28/23')}
-  ];*/
 }
